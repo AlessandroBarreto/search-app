3d8acbd8eb7aa3cd1fc83d068903c160962d355d
alessandrobarreto <alessandrobarreto1201@gmail.com>
2022-01-13 14:49:53 +0000

    feature: navbar component

---
+++ src/components/Routes.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
+
+export const Routes = () => {
+  return (
+    <div className="p-4">
+      <h1>Routes</h1>
+    </div>
+  );
+};

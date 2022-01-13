6106f1cdd492294d76ffca4e292338376aa5d5cc
alessandrobarreto <alessandrobarreto1201@gmail.com>
2022-01-13 15:29:08 +0000

    feature: routes

--- src/components/Routes.jsx
+++ src/components/Routes.jsx
@@ -1,16 +1,18 @@
 import React from "react";
 import { Routes as Switch, Route, Navigate } from "react-router-dom";
+import { Results } from "./Results";
 
 export const Routes = () => {
+  const paths = ["/search", "/images", "/news", "/videos"];
+
   return (
     <div className="p-4">
       <Switch>
-        <Route path="/" />
-        <Route
-          path={["/search,", "/images", "/news", "/videos"]}
-          element={<Results />}
-        />
-        <Route path="*" element={<Navigate to="/search" />} />
+        <Route exact path="/" element={<Navigate to="/search" />} />
+
+        {paths.map((path, i) => (
+          <Route key={i} path={path} element={<Results />} />
+        ))}
       </Switch>
     </div>
   );

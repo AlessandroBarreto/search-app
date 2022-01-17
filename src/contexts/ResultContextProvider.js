0dd3ae7fd08c9def83280402a18b3881dcfc2453
alessandrobarreto <alessandrobarreto1201@gmail.com>
2022-01-17 17:26:23 +0000

    feature: env

--- src/contexts/ResultContextProvider.js
+++ src/contexts/ResultContextProvider.js
@@ -18,7 +18,7 @@ export const ResultContextProvider = ({ children }) => {
         "x-user-agent": "desktop",
         "x-proxy-location": "US",
         "x-rapidapi-host": "google-search3.p.rapidapi.com",
-        "x-rapidapi-key": "e0b753332cmsh34bfcde52ae1cc0p1d77b5jsnd4f5486591e0",
+        "x-rapidapi-key": process.env.REACT_APP_API_KEY,
       },
     });
     const data = await response.json();
